f454df8b8827a9a2f7dbdbf4a637818c0cf71873
EmoryHuang <80020740+EmoryHuang@users.noreply.github.com>
2021-05-02 13:40:23 +0800

    Add files via upload

--- runtime.js
+++ runtime.js
@@ -1,7 +1,7 @@
 //运行时间bynote.cn
 var now = new Date(); 
 function createtime() { 
-    var grt= new Date("12/31/2018 17:38:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
+    var grt= new Date("03/03/2021 12:00:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
     now.setTime(now.getTime()+250); 
     days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days); 
     hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours); 
@@ -9,7 +9,7 @@ function createtime() {
     mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;} 
     seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum); 
     snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;} 
-    document.getElementById("timeDate").innerHTML = "本站已各种夹缝中安全运行 "+dnum+" 天 "; 
+    document.getElementById("timeDate").innerHTML = "本站已安全运行 "+dnum+" 天 "; 
     document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒"; 
 } 
 setInterval("createtime()",250);
